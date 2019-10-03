7d7af1ac0567889ba16bf34df02af1220c597c13
EliminatorBernStudio <56135282+EliminatorBernStudio@users.noreply.github.com>
2019-10-03 22:00:27 +0200

    ProjektInit

---
+++ Assets/PlayerInputController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+
+    public GameObject Car;
+    public string Forward;
+    public string Left;
+    public string Right;
+    public string Down;
+
+    private autocontroller _AutoController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _AutoController = Car.GetComponent<autocontroller>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton(Forward))
+        {
+            _AutoController.Forward = true;
+        } else
+        {
+            _AutoController.Forward = false;
+        }
+        if (Input.GetButton(Left))
+        {
+            _AutoController.Left = true;
+        } else
+        {
+            _AutoController.Left = false;
+        }
+        
+        if (Input.GetButton(Right))
+        {
+            _AutoController.Right = true;
+        } else
+        {
+            _AutoController.Right = false;
+        }
+
+        if (Input.GetButton(Down))
+        {
+            _AutoController.Down = true;
+        }
+        else
+        {
+            _AutoController.Down = false;
+        }
+    }
+}
---
+++ Assets/autocontroller.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class autocontroller : MonoBehaviour
+{
+
+    public int Speed;
+    public int RotationSpeed;
+    public bool Forward;
+    public bool Left;
+    public bool Right;
+    public bool Down;
+
+    private GameObject Car;
+    private Rigidbody _RigidbodyCar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Car = this.gameObject;
+        _RigidbodyCar = Car.GetComponent<Rigidbody>();
+        Debug.Log(_RigidbodyCar);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var TimePastSinceLastFrame = Time.deltaTime;
+        var DistanceToTravel = Speed * TimePastSinceLastFrame;
+       
+        
+        if (Forward)
+        {
+            //Car.transform.position += Car.transform.forward * DistanceToTravel;
+            _RigidbodyCar.AddForce(Car.transform.forward * DistanceToTravel, ForceMode.VelocityChange);
+        }
+
+        if (Left)
+        {
+            Car.transform.Rotate(0, Time.deltaTime + -DistanceToTravel, 0, Space.Self);
+           
+        }
+
+        if (Right)
+        {
+            Car.transform.Rotate(0, Time.deltaTime + DistanceToTravel, 0, Space.Self);
+        }
+        if (Down)
+        {
+            //Car.transform.position += Car.transform.forward * DistanceToTravel;
+            _RigidbodyCar.AddForce(Car.transform.forward * -DistanceToTravel, ForceMode.VelocityChange);
+        }
+    }
+}
