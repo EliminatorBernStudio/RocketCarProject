55560507b1f1485823e41dc58c417472873df3cd
EliminatorBernStudio <56135282+EliminatorBernStudio@users.noreply.github.com>
2019-10-03 22:53:18 +0200

    update

---
+++ Assets/ResetSetting.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class ResetSetting : MonoBehaviour
+{
+    [MenuItem ("Edit/Reset Preferences")]
+	public static void ResetPrefs() {
+		if(EditorUtility.DisplayDialog("Reset editor preferences?", "Reset all editor preferences? This cannot be undone.", "Yes", "No")) {
+			EditorPrefs.DeleteAll();
+		}
+	}
+}
